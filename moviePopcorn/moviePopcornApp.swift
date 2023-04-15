b8d2f7216d1872dcf9a3b268ea1558d17c1dd594
Petar <petardjurickovic@gmail.com>
2023-04-15 16:43:29 +0200

    Initial Commit

---
+++ moviePopcorn/moviePopcornApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviePopcornApp.swift
+//  moviePopcorn
+//
+//  Created by Obuka on 15.4.23..
+//
+
+import SwiftUI
+
+@main
+struct moviePopcornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
